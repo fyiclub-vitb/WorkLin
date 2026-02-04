a7b6fdf8c0d4dc12622d46f378265c4c0442fc47
Parth Gupta <guptaparth2209@gmail.com>
2026-02-04 12:36:42 +0530

    mobile index.ts creatd

---
+++ src/components/mobile/index.ts
@@ -0,0 +1,11 @@
+// Mobile Components Export
+// This file exports all mobile-specific components for easy importing
+
+export { MobileBottomNav } from './MobileBottomNav';
+export { MobileHeader } from './MobileHeader';
+export { MobileSidebarDrawer } from './MobileSidebarDrawer';
+export { SwipeableCard } from './SwipeableCard';
+export { PullToRefresh } from './PullToRefresh';
+export { InstallPrompt } from './InstallPrompt';
+export { UpdatePrompt } from './UpdatePrompt';
+
